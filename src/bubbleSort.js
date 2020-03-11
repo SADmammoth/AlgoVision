9b54be3f40c4adf94be8cb17a6530a7c917da525
SADmammoth <lo.ma200018@gmail.com>
2020-03-11 16:46:20 +0300

    feat: bubble sort visualisation

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,10 +1,13 @@
-function bubbleSort(arr) {
+async function bubbleSort(arr, onCompare=(array,left,right)=>{}, onSwap=(array,left,right)=>{}) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
+          console.log(arr.length-i);
+          await onCompare(arr, arr[j], arr[j+1]);
           if(arr[j] > arr[j+1]){
             let temp = arr[j+1];
             arr[j+1] = arr[j];
             arr[j] = temp;
+            await onSwap(arr, arr[j], arr[j+1]);
         }
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
+// (async function(){
+//     let arr =  generateArray(2,20, 10).map((el,i)=>({id: i, valueOf: ()=>Math.round(el)}));
+    
+//    arr.forEach(el=> { document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
+//    quickSort(arr, async (array,l,r)=>{
+       
+// await new Promise(resolve=>setTimeout(()=>resolve(), 500)); 
+//        document.getElementById(l.id).style.background ='red';
+//        await new Promise(resolve=>setTimeout(()=>resolve(), 1000));  
+       
+//     document.getElementById('diag').innerHTML = ''; 
+//     array.forEach(el=> {document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
+//      document.getElementById(l.id).style.background ='black';
+//      return new Promise(resolve=>resolve());
+// },
+// async (array,l,r)=> {
+    
+// await new Promise(resolve=>setTimeout(()=>resolve(), 500));  
+// document.getElementById(l.id).style.background ='green';
+//  document.getElementById(r.id).style.background ='green';
+// await new Promise(resolve=>setTimeout(()=>resolve(), 1000));  
+// document.getElementById('diag').innerHTML = ''; 
+// array.forEach(el=> {document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
+// document.getElementById(l.id).style.background ='black';
+// document.getElementById(r.id).style.background ='black';
+// return new Promise(resolve=>resolve());
+// },
+
+// async (array,pivot)=> {
+//     [...document.getElementById('diag').children].forEach(el=>el.style.background = 'black');
+//     if(pivot){
+//     document.getElementById(pivot.id).style.background ='blue';
+//     return new Promise(resolve=>resolve());
+//     }
+// },
+// async (array,left,right)=> {
+//     [...document.getElementById('diag').children].forEach(el=>el.style.opacity = 0.4);
+//     array.slice(left, right+1).forEach(el=>document.getElementById(el.id).style.opacity = 1);
+//     return new Promise(resolve=>resolve());
+// });
+//     window.arr = arr;
+    
+// })()
+
 (async function(){
     let arr =  generateArray(2,20, 10).map((el,i)=>({id: i, valueOf: ()=>Math.round(el)}));
     
    arr.forEach(el=> { document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
-   quickSort(arr, async (array,l,r)=>{
+   bubbleSort(arr, async (array,l,r)=>{
        
 await new Promise(resolve=>setTimeout(()=>resolve(), 500)); 
        document.getElementById(l.id).style.background ='red';
@@ -24,24 +68,7 @@ array.forEach(el=> {document.getElementById('diag').appendChild(createElement(el
 document.getElementById(l.id).style.background ='black';
 document.getElementById(r.id).style.background ='black';
 return new Promise(resolve=>resolve());
-},
-
-async (array,pivot)=> {
-    [...document.getElementById('diag').children].forEach(el=>el.style.background = 'black');
-    if(pivot){
-    document.getElementById(pivot.id).style.background ='blue';
-    return new Promise(resolve=>resolve());
-    }
-},
-async (array,left,right)=> {
-    [...document.getElementById('diag').children].forEach(el=>el.style.opacity = 0.4);
-    array.slice(left, right+1).forEach(el=>document.getElementById(el.id).style.opacity = 1);
-    return new Promise(resolve=>resolve());
-});
-    window.arr = arr;
-    
-})()
-
+})})()
 
 function createElement(height, id){
     let div = document.createElement('div');
