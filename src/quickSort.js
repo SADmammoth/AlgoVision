8b451ebac6a7fa1a3087895c19f475b6dc0ad608
SADmammoth <lo.ma200018@gmail.com>
2020-03-11 16:44:37 +0300

    feat: quick sort visualisation

--- src/quickSort.js
+++ src/quickSort.js
@@ -1,29 +1,42 @@
-function quickSort(array) {
+async function quickSort(array, 
+  onСompare=(array, left, right)=>{}, 
+  onSwap=(array, fst, snd)=>{}, 
+  onMidChange=(array,mid)=>{},
+  onGroupChange=(array,left,right)) {
     let arr = [...array];
     let temp;
     let i;
     let j;
     let mid;
-    function sort(arr, left, right) {
+    async function sort(arr, left, right) {
       i = left;
       j = right;
       mid = arr[Number.parseInt((left + right) / 2)];
+      onMidChange(arr,mid); 
+      onGroupChange(arr, left,right);
       do {
-        while (arr[i] < mid) i++;
-        while (arr[j] > mid) j--;
+        while (arr[i].valueOf() < mid.valueOf()) {await onСompare(arr, arr[i], mid); onMidChange(arr,mid);     onGroupChange(arr, left,right);i++};
+        while (arr[j].valueOf() > mid.valueOf()) {await onСompare(arr, arr[j], mid);  onMidChange(arr,mid);     onGroupChange(arr, left,right);j--};
   
         if (i <= j) {
+     
           temp = arr[i];
           arr[i] = arr[j];
           arr[j] = temp;
+          await onSwap(arr, arr[i], arr[j]);
+          
+          onMidChange(arr,mid);
+          onGroupChange(arr, left,right);
           i++;
           j--;
         }
       } while (i <= j);
   
-      if (i < right) sort(arr, i, right);
-      if (j > left) sort(arr, left, j);
+      if (i < right) await sort(arr, i, right);
+      if (j > left) await sort(arr, left, j);
     }
-    sort(arr, 0, arr.length - 1);
+    await sort(arr, 0, arr.length - 1);
+    onGroupChange(arr);
+    onMidChange(arr);
     return arr;
   }
