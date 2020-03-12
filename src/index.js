b5de95460f11dc24356186b3778050a6a53b0911
SADmammoth <lo.ma200018@gmail.com>
2020-03-12 19:48:49 +0300

    feat: create UI and improve code readability

--- src/index.js
+++ src/index.js
@@ -1,83 +1,154 @@
-// (async function(){
-//     let arr =  generateArray(2,20, 10).map((el,i)=>({id: i, valueOf: ()=>Math.round(el)}));
-    
-//    arr.forEach(el=> { document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
-//    quickSort(arr, async (array,l,r)=>{
-       
-// await new Promise(resolve=>setTimeout(()=>resolve(), 500)); 
-//        document.getElementById(l.id).style.background ='red';
-//        await new Promise(resolve=>setTimeout(()=>resolve(), 1000));  
-       
-//     document.getElementById('diag').innerHTML = ''; 
-//     array.forEach(el=> {document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
-//      document.getElementById(l.id).style.background ='black';
-//      return new Promise(resolve=>resolve());
-// },
-// async (array,l,r)=> {
-    
-// await new Promise(resolve=>setTimeout(()=>resolve(), 500));  
-// document.getElementById(l.id).style.background ='green';
-//  document.getElementById(r.id).style.background ='green';
-// await new Promise(resolve=>setTimeout(()=>resolve(), 1000));  
-// document.getElementById('diag').innerHTML = ''; 
-// array.forEach(el=> {document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
-// document.getElementById(l.id).style.background ='black';
-// document.getElementById(r.id).style.background ='black';
-// return new Promise(resolve=>resolve());
-// },
-
-// async (array,pivot)=> {
-//     [...document.getElementById('diag').children].forEach(el=>el.style.background = 'black');
-//     if(pivot){
-//     document.getElementById(pivot.id).style.background ='blue';
-//     return new Promise(resolve=>resolve());
-//     }
-// },
-// async (array,left,right)=> {
-//     [...document.getElementById('diag').children].forEach(el=>el.style.opacity = 0.4);
-//     array.slice(left, right+1).forEach(el=>document.getElementById(el.id).style.opacity = 1);
-//     return new Promise(resolve=>resolve());
-// });
-//     window.arr = arr;
-    
-// })()
-
-(async function(){
-    let arr =  generateArray(2,20, 10).map((el,i)=>({id: i, valueOf: ()=>Math.round(el)}));
-    
-   arr.forEach(el=> { document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
-   bubbleSort(arr, async (array,l,r)=>{
-       
-await new Promise(resolve=>setTimeout(()=>resolve(), 500)); 
-       document.getElementById(l.id).style.background ='red';
-       await new Promise(resolve=>setTimeout(()=>resolve(), 1000));  
-       
-    document.getElementById('diag').innerHTML = ''; 
-    array.forEach(el=> {document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
-     document.getElementById(l.id).style.background ='black';
-     return new Promise(resolve=>resolve());
-},
-async (array,l,r)=> {
-    
-await new Promise(resolve=>setTimeout(()=>resolve(), 500));  
-document.getElementById(l.id).style.background ='green';
- document.getElementById(r.id).style.background ='green';
-await new Promise(resolve=>setTimeout(()=>resolve(), 1000));  
-document.getElementById('diag').innerHTML = ''; 
-array.forEach(el=> {document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id))});
-document.getElementById(l.id).style.background ='black';
-document.getElementById(r.id).style.background ='black';
-return new Promise(resolve=>resolve());
-})})()
-
-function createElement(height, id){
-    let div = document.createElement('div');
-    div.id = id;
-    div.classList.add('element');
-    div.style.setProperty('--i', height);
-    return div;
+function createElement(height, id) {
+  console.log(height);
+  let div = document.createElement('div');
+  div.id = id;
+  div.classList.add('element');
+  div.style.setProperty('--i', height);
+  return div;
 }
 
-function generateArray(value_min, value_max, size){
-    return new Array(size).fill(null).map(()=>(Math.floor(Math.random() * Math.floor(value_max-1))+value_min)); 
+function generateArray(value_min, value_max, size) {
+  return new Array(size).fill(null).map(() => Math.floor(Math.random() * Math.floor(value_max - 1)) + value_min);
+}
+
+function renderArray(arr) {
+  document.getElementById('diag').innerHTML = '';
+
+  arr.forEach((el) => {
+    document.getElementById('diag').appendChild(createElement(el.valueOf(), el.id));
+  });
+}
+
+function comparedElement(el) {
+  document.getElementById(el.id).style.background = 'red';
+}
+
+(function() {
+  let array_input = document.getElementById('array_input');
+  array_input.value = generateArray(2, 20, 10);
+
+  let form = document.getElementById('sort_form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let { array, sort } = event.target.elements;
+
+    if (array.value === '' || sort.value === '' || !/^([0-9]+[, ][ ]?)*([0-9]+)$/.test(array.value)) {
+      alert('Wrong input');
+      return;
+    }
+
+    let sortAlgo = () => {};
+    array = array.value.match(/([0-9]+)(?=(?=[, ][ ]?)|$)/g).map((el, i) => ({ id: i, valueOf: () => parseInt(el) }));
+
+    switch (sort.value) {
+      case 'quick': {
+        sortAlgo = () =>
+          VisualQuickSort(
+            array,
+            renderArray,
+            (el) => {
+              let normal = document.getElementById(el.id);
+              normal.style.opacity = 0.4;
+              normal.style.background = 'black';
+              return normal;
+            },
+            (el) => (document.getElementById(el.id).style.background = 'red'),
+            (el) => (document.getElementById(el.id).style.background = 'green'),
+            (el) => (document.getElementById(el.id).style.background = 'blue'),
+            (el) => (document.getElementById(el.id).style.opacity = 1),
+          );
+        break;
+      }
+      case 'bubble': {
+        sortAlgo = () =>
+          VisualBubbleSort(
+            array,
+            renderArray,
+            (el) => (document.getElementById(el.id).style.background = 'black'),
+            (el) => (document.getElementById(el.id).style.background = 'red'),
+            (el) => (document.getElementById(el.id).style.background = 'green'),
+          );
+        break;
+      }
+    }
+
+    sortAlgo();
+  });
+})();
+
+function VisualQuickSort(
+  array,
+  renderArray = (array) => array,
+  normalElement = (el) => el,
+  comparedElement = (el) => el,
+  swappedElement = (el) => el,
+  pivotElement = (el) => el,
+  groopedElement = (el) => el,
+) {
+  renderArray(array);
+
+  async function onCompare(array, left, right) {
+    await new Promise((resolve) => setTimeout(() => resolve(), 500));
+    comparedElement(left);
+    await new Promise((resolve) => setTimeout(() => resolve(), 1000));
+    normalElement(left);
+    renderArray(array);
+    return new Promise((resolve) => resolve());
+  }
+
+  async function onSwap(array, left, right) {
+    await new Promise((resolve) => setTimeout(() => resolve(), 500));
+    swappedElement(left);
+    swappedElement(right);
+    await new Promise((resolve) => setTimeout(() => resolve(), 1000));
+    normalElement(left);
+    normalElement(right);
+    renderArray(array);
+    return new Promise((resolve) => resolve());
+  }
+
+  async function onMidChange(array, pivot) {
+    array.forEach((el) => normalElement(el));
+    pivotElement(pivot);
+    return new Promise((resolve) => resolve());
+  }
+
+  async function onRegroup(array, left, right) {
+    array.slice(left, right + 1).forEach((el) => groopedElement(el));
+    return new Promise((resolve) => resolve());
+  }
+
+  quickSort(array, onCompare, onSwap, onMidChange, onRegroup);
+}
+
+function VisualBubbleSort(
+  array,
+  renderArray = (array) => array,
+  normalElement = (el) => el,
+  comparedElement = (el) => el,
+  swappedElement = (el) => el,
+) {
+  renderArray(array);
+
+  async function onCompare(array, left, right) {
+    comparedElement(left);
+    comparedElement(right);
+    await new Promise((resolve) => setTimeout(() => resolve(), 500));
+    normalElement(left);
+    normalElement(right);
+    renderArray(array);
+    return new Promise((resolve) => resolve());
+  }
+
+  async function onSwap(array, left, right) {
+    swappedElement(left);
+    swappedElement(right);
+    await new Promise((resolve) => setTimeout(() => resolve(), 500));
+    normalElement(left);
+    normalElement(right);
+    renderArray(array);
+    return new Promise((resolve) => resolve());
+  }
+
+  bubbleSort(array, onCompare, onSwap);
 }
--- src/quickSort.js
+++ src/quickSort.js
@@ -1,42 +1,53 @@
-async function quickSort(array, 
-  onСompare=(array, left, right)=>{}, 
-  onSwap=(array, fst, snd)=>{}, 
-  onMidChange=(array,mid)=>{},
-  onGroupChange=(array,left,right)) {
-    let arr = [...array];
-    let temp;
-    let i;
-    let j;
-    let mid;
-    async function sort(arr, left, right) {
-      i = left;
-      j = right;
-      mid = arr[Number.parseInt((left + right) / 2)];
-      onMidChange(arr,mid); 
-      onGroupChange(arr, left,right);
-      do {
-        while (arr[i].valueOf() < mid.valueOf()) {await onСompare(arr, arr[i], mid); onMidChange(arr,mid);     onGroupChange(arr, left,right);i++};
-        while (arr[j].valueOf() > mid.valueOf()) {await onСompare(arr, arr[j], mid);  onMidChange(arr,mid);     onGroupChange(arr, left,right);j--};
-  
-        if (i <= j) {
-     
-          temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
-          await onSwap(arr, arr[i], arr[j]);
-          
-          onMidChange(arr,mid);
-          onGroupChange(arr, left,right);
-          i++;
-          j--;
-        }
-      } while (i <= j);
-  
-      if (i < right) await sort(arr, i, right);
-      if (j > left) await sort(arr, left, j);
-    }
-    await sort(arr, 0, arr.length - 1);
-    onGroupChange(arr);
-    onMidChange(arr);
-    return arr;
+async function quickSort(
+  array,
+  onСompare = (array, left, right) => {},
+  onSwap = (array, fst, snd) => {},
+  onMidChange = (array, mid) => {},
+  onGroupChange = (array, left, right),
+) {
+  let arr = [...array];
+  let temp;
+  let i;
+  let j;
+  let mid;
+  async function sort(arr, left, right) {
+    i = left;
+    j = right;
+    mid = arr[Number.parseInt((left + right) / 2)];
+    onMidChange(arr, mid);
+    onGroupChange(arr, left, right);
+    do {
+      while (arr[i].valueOf() < mid.valueOf()) {
+        await onСompare(arr, arr[i], mid);
+        onMidChange(arr, mid);
+        onGroupChange(arr, left, right);
+        i++;
+      }
+      while (arr[j].valueOf() > mid.valueOf()) {
+        await onСompare(arr, arr[j], mid);
+        onMidChange(arr, mid);
+        onGroupChange(arr, left, right);
+        j--;
+      }
+
+      if (i <= j) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        await onSwap(arr, arr[i], arr[j]);
+
+        onMidChange(arr, mid);
+        onGroupChange(arr, left, right);
+        i++;
+        j--;
+      }
+    } while (i <= j);
+
+    if (i < right) await sort(arr, i, right);
+    if (j > left) await sort(arr, left, j);
   }
+  await sort(arr, 0, arr.length - 1);
+  onGroupChange(arr);
+  onMidChange(arr);
+  return arr;
+}
